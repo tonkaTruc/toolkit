c6c7ad1c3350b98268c441cf5ecad1a71ce97fac
Tommy Sutton <tommy.sutton@live.com>
2021-11-09 21:30:53 +0000

    Converting all tabs to spaces

--- Toolkit/multicast.py
+++ Toolkit/multicast.py
@@ -6,112 +6,112 @@ from .utils import get_best_interface_for
 
 class MulticastMgr:
 
-	def __init__(self, switch_ip=None):
+  def __init__(self, switch_ip=None):
 
-		if not switch_ip: print("Switch IP address is missing... initialise using \"switch_ip=xx.xx.xx.xx\"")
-		self.local_ip = get_best_interface_for(switch_ip)
+    if not switch_ip: print("Switch IP address is missing... initialise using \"switch_ip=xx.xx.xx.xx\"")
+    self.local_ip = get_best_interface_for(switch_ip)
 
-		self.sock = create_socket(self.local_ip, 9989)
-		print(f'Using interface: {self.local_ip}')
-		print(f'Created UDP socket: {self.sock}')
+    self.sock = create_socket(self.local_ip, 9989)
+    print(f'Using interface: {self.local_ip}')
+    print(f'Created UDP socket: {self.sock}')
 
-		# set multicast interface to local_ip
-		self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(self.local_ip))
+    # set multicast interface to local_ip
+    self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(self.local_ip))
 
-		# Set multicast time-to-live to 2... keep our multicast packets from escaping the local network
-		self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
+    # Set multicast time-to-live to 2... keep our multicast packets from escaping the local network
+    self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 
-	def join(self, multicast_ip):
-		# Construct a membership request...
-		mreq = struct.pack("4sl", socket.inet_aton(multicast_ip), socket.INADDR_ANY)
-		# Send add membership request to socket
-		self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+  def join(self, multicast_ip):
+    # Construct a membership request...
+    mreq = struct.pack("4sl", socket.inet_aton(multicast_ip), socket.INADDR_ANY)
+    # Send add membership request to socket
+    self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
-	def leave(self, multicast_ip):
-		mreq = struct.pack("4sl", socket.inet_aton(multicast_ip), socket.INADDR_ANY)
-		self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
+  def leave(self, multicast_ip):
+    mreq = struct.pack("4sl", socket.inet_aton(multicast_ip), socket.INADDR_ANY)
+    self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
 
-	def display_traffic(self):
-		while True: print(self.sock.recv(1))
+  def display_traffic(self):
+    while True: print(self.sock.recv(1))
 
 
 def create_socket(ip, port):
-	"""
-	Returns an open socket, bound to the NIC with given IP address.
-	@port is the port that is bound with the local ip address to create the socket
-	"""
+  """
+  Returns an open socket, bound to the NIC with given IP address.
+  @port is the port that is bound with the local ip address to create the socket
+  """
 
-	# create UDP socket
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  # create UDP socket
+  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-	# allow reuse of addresses
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  # allow reuse of addresses
+  sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-	# If you bind to a specific interface on the Mac, no multicast data will arrive.
-	# If you try to bind to all interfaces on Windows, no multicast data will arrive.
-	# Hence the following.
-	sock.bind(('0.0.0.0', port)) if sys.platform.startswith("darwin") else sock.bind((ip, port))
-	return sock
+  # If you bind to a specific interface on the Mac, no multicast data will arrive.
+  # If you try to bind to all interfaces on Windows, no multicast data will arrive.
+  # Hence the following.
+  sock.bind(('0.0.0.0', port)) if sys.platform.startswith("darwin") else sock.bind((ip, port))
+  return sock
 
 def ip_is_local(ip_string):
-	"""
-	Uses a regex to determine if the input ip is on a local network. Returns a boolean.
-	It's safe here, but never use a regex for IP verification if from a potentially dangerous source.
-	"""
-	combined_regex = "(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)"
-	
-	return re.match(combined_regex, ip_string) is not None # convert to a boolean
+  """
+  Uses a regex to determine if the input ip is on a local network. Returns a boolean.
+  It's safe here, but never use a regex for IP verification if from a potentially dangerous source.
+  """
+  combined_regex = "(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)"
+  
+  return re.match(combined_regex, ip_string) is not None # convert to a boolean
 
 def print_stream_info(socket):
 
-	# Data waits on socket buffer until we retrieve it.
-	# NOTE: Normally, you would want to compare the incoming data's source address to your own, and filter it out
-	#       if it came from the current machine. Everything you send gets echoed back at you if your socket is
-	#       subscribed to the multicast group.
+  # Data waits on socket buffer until we retrieve it.
+  # NOTE: Normally, you would want to compare the incoming data's source address to your own, and filter it out
+  #       if it came from the current machine. Everything you send gets echoed back at you if your socket is
+  #       subscribed to the multicast group.
 
-	# Test Var
-	multicast_ip = "239.1.2.3"
+  # Test Var
+  multicast_ip = "239.1.2.3"
 
-	packet = socket.recvfrom(2624)
+  packet = socket.recvfrom(2624)
 
-	# packet tuple to string
-	packet = packet[0]
+  # packet tuple to string
+  packet = packet[0]
 
-	# Take first 20 characters as IP header
-	ip_header = packet[0:20]
+  # Take first 20 characters as IP header
+  ip_header = packet[0:20]
  
-	iph = struct.unpack('!BBHHHBBH4s4s' , ip_header)
+  iph = struct.unpack('!BBHHHBBH4s4s' , ip_header)
 
-	version_ihl = iph[0]
-	version = version_ihl >> 4
-	ihl = version_ihl & 0xF
-	iph_length = ihl * 4
-	protocol = iph[6]
+  version_ihl = iph[0]
+  version = version_ihl >> 4
+  ihl = version_ihl & 0xF
+  iph_length = ihl * 4
+  protocol = iph[6]
 
-	u = iph_length
-	udph_length = 8
-	udp_header = packet[u:u+udph_length]
+  u = iph_length
+  udph_length = 8
+  udp_header = packet[u:u+udph_length]
  
-	# now unpack them :)
-	udph = struct.unpack('!HHHH' , udp_header)
+  # now unpack them :)
+  udph = struct.unpack('!HHHH' , udp_header)
 
-	source_port = udph[0]
-	dest_port = udph[1]
+  source_port = udph[0]
+  dest_port = udph[1]
 
-	if protocol == 17:
-		protocol = 'UDP'
+  if protocol == 17:
+    protocol = 'UDP'
 
-	src_addr = socket.inet_ntoa(iph[8]);
-	dst_addr = socket.inet_ntoa(iph[9]);
+  src_addr = socket.inet_ntoa(iph[8]);
+  dst_addr = socket.inet_ntoa(iph[9]);
 
-	if dst_addr == multicast_ip:
-		print('\nMulticast stream detected!')
-		print('\nProtocol: \t\t', protocol)
-		print('Source Port: \t\t', source_port, '\nDestination Port: \t', dest_port)
-		print('Source Address: \t', src_addr, '\nDestination Address: \t', dst_addr)
-		print('\nCheck wireshark to ensure the correct packets are reaching your NIC\n')
-		return True
+  if dst_addr == multicast_ip:
+    print('\nMulticast stream detected!')
+    print('\nProtocol: \t\t', protocol)
+    print('Source Port: \t\t', source_port, '\nDestination Port: \t', dest_port)
+    print('Source Address: \t', src_addr, '\nDestination Address: \t', dst_addr)
+    print('\nCheck wireshark to ensure the correct packets are reaching your NIC\n')
+    return True
 
-	else:
-		print('No Multicast stream present at: ', multicast_ip)
-		return False
+  else:
+    print('No Multicast stream present at: ', multicast_ip)
+    return False
--- tasks.py
+++ tasks.py
@@ -5,32 +5,32 @@ from Toolkit.capture import CaptureMgr
 
 @task
 def multicast_join(c, addr):
-	"""Send a multicast group join request and loop forever"""
-	
-	# Assign a manager by supplying the IP of the target switch device
-	mgr = MulticastMgr(switch_ip="192.168.0.1")
-	mgr.join(addr)
+  """Send a multicast group join request and loop forever"""
+  
+  # Assign a manager by supplying the IP of the target switch device
+  mgr = MulticastMgr(switch_ip="192.168.0.1")
+  mgr.join(addr)
 
-	try:
-		print(f'Multicast JOIN has been sent for {addr}. Entering while loop... Ctrl+C to exit and close socket.')
-		while True: pass
-	except KeyboardInterrupt:
-		mgr.leave(addr)
+  try:
+    print(f'Multicast JOIN has been sent for {addr}. Entering while loop... Ctrl+C to exit and close socket.')
+    while True: pass
+  except KeyboardInterrupt:
+    mgr.leave(addr)
 
 @task
 def server_local_serve(c, port):
-	""" Serves a basic connection point to localhost on a given port"""
-	SimpleServer("127.0.0.1", port).serve()
+  """ Serves a basic connection point to localhost on a given port"""
+  SimpleServer("127.0.0.1", port).serve()
 
 @task
 def capture(c, interface=None, count=20):
-	"""Capture network traffic on a specified interface"""
-	
-	cap = CaptureMgr(capture_int=interface)
-	try:
-		cap.capture(count=count)
-	except KeyboardInterrupt:
-		quit()
+  """Capture network traffic on a specified interface"""
+  
+  cap = CaptureMgr(capture_int=interface)
+  try:
+    cap.capture(count=count)
+  except KeyboardInterrupt:
+    quit()
 
 @task
 def sniff(c):
